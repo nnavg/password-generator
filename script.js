68923ad93dcfefdb4abb7670a9d7cc4c4af8a6ec
Nav Gill <nsgngng@gmail.com>
2022-10-28 12:58:56 -0400

    second commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+function generatePassword(){
+  writePassword();
+}
+
+// Write password to the #password input
+function writePassword() {
+  var length = prompt('Please choose your password length from 8 to 128 characters.')
+  var newlength = parseInt(length);
+  if (isNaN(newlength), newlength<8, newlength>128) {
+    alert("error");
+    return;
+  }
+  
+  const alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const alphaLower = "acdefghijklmnopqrstuvwxyz";
+  const numbers = "012345678989";
+  const symbols = "!@#$%^&*_-+=";
+
+  var useUpperCase = prompt("Would you like to include uppercase letters?  yes/no");
+  var useLowerCase = prompt("Would you like to include lowercase letters?  yes/no");
+  var useSpecial = prompt("Would you like to include speical characters?  yes/no");
+  var useNumeric = prompt("Would you like to include numeric characters?  yes/no");
+  
+
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
